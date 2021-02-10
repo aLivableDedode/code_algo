8fab9b14d7c1218154900c16aa6f23d2d599cdb5
aLivableDedode <535074817@qq.com>
2021-02-10 15:10:40 +0800

    #补充

--- algo_data_structure/src/main/java/monotonicstack/CircularArray.java
+++ algo_data_structure/src/main/java/monotonicstack/CircularArray.java
@@ -3,6 +3,9 @@ package monotonicstack;
 import java.util.Stack;
 
 /**
+ *  给你一个数组T，这个数组存放的是近几天的天气气温，你返回一个等长的数组，
+ *     计算：对于每一天，你还要至少等多少天才能等到一个更暖和的气温；如果等不到那一天，填 0。
+ *
  * @Author lvkai
  * @Description
  * @Date 2021/1/21 21:53
